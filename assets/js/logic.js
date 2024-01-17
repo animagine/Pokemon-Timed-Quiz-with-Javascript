e24ef598242e02b649afa0c9f187c5e166758773
animagine <animagine@live.com>
2024-01-17 10:40:39 +0000

    added questions to quiz and added event listner

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,8 +13,11 @@ var questionsPage = document.getElementById('questions');
 var questions = document.getElementById('question-title');
 var choices = document.getElementById('choices');
 
+//Defiining audio variables
+var correctsound = new Audio('./assets/sfx/correct.wav');
+var incorrectsound = new Audio('./assets/sfx/incorrect.wav');
 
-// Initialize quiz timer
+// Initialising quiz timer
 var timeLeft = 100; 
 function countdown() {
     // Using 'setInterval()' method to count down every 1000ms
@@ -29,3 +32,80 @@ function countdown() {
         }
     }, 1000);
 };
+
+//Setting up quiz funtionality
+function Quiz() {
+   
+    let currentQuestionIndex = 0;
+
+    // Function to display the current question and options
+    function displayQuestion() {
+        if (currentQuestionIndex < Quiz.length) {
+            // Get the current question and options
+            const currentQuestion = Quiz[currentQuestionIndex];
+            const questionOutput = currentQuestion.question;
+            const answerChoices = currentQuestion.answers;
+            const correctChoice = currentQuestion.correctAnswer;
+
+            // Display the question
+            questions.textContent = questionOutput;
+
+            // Render a list of buttons for the options
+            choices.innerHTML = "";
+            for (let i = 0; i < answerChoices.length; i++) {
+                const li = document.createElement("li");
+                const choiceBtn = document.createElement("button");
+                choiceBtn.textContent = answerChoices[i];
+
+                // Add a click event listener to the button to move to next question
+                choiceBtn.addEventListener('click', function () {
+                    // Register user's choice
+                    var userChoice = choiceBtn.textContent;
+                    if(userChoice === correctChoice) {
+                        //feedback correct
+                        feedback.setAttribute("class","feedback start");
+                        feedback.textContent = "Correct!"
+                        //Play correct audio
+                        correctsound.play();
+                    } else {
+                        //feedback wrong!
+                        feedback.setAttribute("class","feedback start");
+                        feedback.textContent = "Wrong!"
+                        //cut time by 10s
+                        timeLeft = timeLeft - 10;
+                        //Play incorrect audio
+                        incorrectsound.play();
+                    };
+
+                    
+                    setTimeout(function(){
+                        feedback.setAttribute("class","feedback hide");
+                    }, 1000); 
+
+                    currentQuestionIndex++;
+                    displayQuestion();
+                });
+
+                li.appendChild(choiceBtn);
+                choices.appendChild(li);
+            }
+        } else {
+            // Quiz end
+            finalScore = timeLeft;
+            console.log("Quiz finished");
+            console.log("The final score is "+ finalScore);
+            endGame();
+        }
+    }
+
+    displayQuestion();
+}
+
+//Event listener to trigger quiz start and timer
+start.addEventListener("click",function() {
+    countdown();    //When clicked start the countdown
+    startScreen.setAttribute("class","hide"); //Hide start-screen
+    questionsPage.setAttribute("class","start"); //Move on to the questions in questions.js
+    Quiz();
+});
+
---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// Questions and answers for quiz
+var Quiz = [
+    {
+        question: "1. Pokemon is short for what?:",
+        answers: ['1. Pocket Money','2. Pocket Monsters','3. Pork Meat','4. Pocket Moon'],
+        correctAnswer: '2. Pocket Monsters'
+    },
+    {
+        question: "2. What was Ash's first Pokemon ?",
+        answers: ['1. Bulbasur','2. Charmander','3. Squirtle','4. Pickachu'],
+        correctAnswer: '4. Pickachu'
+    },
+    {
+        question: "3. What type of Pokemon is Pickachu",
+        answers: ['1. Water Pokemon','2. Electric Pokemon','3. Rock Pokemon','4. Fire Pokemon'],
+        correctAnswer: '2. Electric Pokemon'
+    },
+    {
+        question: "4. what is the color of Pickachu?.",
+        answers: ['1. Red','2. Green','3. Blue','4. Yellow'],
+        correctAnswer: '4. Yellow'
+    },
+    {
+        question: "5. Who is Ash's best friend ?",
+        answers: ['1. Pickachu','2. Misty','3. Brock','4. Mew'],
+        correctAnswer: '1. Pickachu'
+    },
+];
