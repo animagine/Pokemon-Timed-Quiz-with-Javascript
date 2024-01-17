4885feccb602a6488353c36b11843ef0c5d149ea
animagine <animagine@live.com>
2024-01-17 11:15:09 +0000

    added variables for highscores

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//Get variables from highscores.html
+var highscores = document.getElementById('highscores');
+var clear = document.getElementById('clear');
+UserScore = JSON.parse(localStorage.getItem("score"));
+const li = document.createElement("li");
+li.textContent = UserScore[0] + ' - ' + UserScore[1];
+highscores.appendChild(li);
+
+clear.addEventListener("click", function() {
+    highscores.removeChild(li);
+    localStorage.setItem("score","");
+});
