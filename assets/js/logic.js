4e03ba9a91d084fe85a629a14065e372a81ed090
animagine <animagine@live.com>
2024-01-17 11:18:07 +0000

    fixed syntaax error on line 4

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,7 @@
 // Getting variables from the HTML document using the DOM
 
 var timer = document.getElementById('time'); 
-var timerText = document.getElementById('Timer');
+var timerText = document.getElementById('timer');
 var startScreen = document.getElementById('start-screen');
 var start = document.getElementById('start');
 var endScreen = document.getElementById('end-screen');
