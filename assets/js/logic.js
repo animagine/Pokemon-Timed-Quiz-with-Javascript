d6a68838fafab7e993aa196d237891f1949318ee
animagine <animagine@live.com>
2024-01-17 09:08:22 +0000

    Get HTML elements with DOM

---
+++ assets/js/logic.js
@@ -0,0 +1,14 @@
+// Getting variables from the HTML document using the DOM
+
+var timer = document.getElementById('time'); 
+var timerText = document.getElementById('Timer');
+var startScreen = document.getElementById('start-screen');
+var start = document.getElementById('start');
+var endScreen = document.getElementById('end-screen');
+var score = document.getElementById('final-score');
+var initials = document.getElementById('initials');
+var submit = document.getElementById('submit');
+var feedback = document.getElementById('feedback'); 
+var questionsPage = document.getElementById('questions');
+var questions = document.getElementById('question-title');
+var choices = document.getElementById('choices'); 
